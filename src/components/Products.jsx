e96ddcc7481e7c4ab8934b9012875f4f8126cbc2
Bikash Adhikari <bikasha@Bikashs-MacBook-Air.local>
2022-06-30 11:05:05 +0530

    api integration in the product components

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,10 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 import styled from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
+
 const Container = styled.div`
     padding: 20px;
     display: flex;
@@ -15,12 +17,36 @@ const Products = ({cat, filters, sort}) => {
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
+    let tokst = localStorage.getItem("token");
+    if (tokst !== "undefined") {
+      var token = JSON.parse(localStorage.getItem("token"));
+    }
+    const getProducts = async() => {
+      try{
+        const res = await axios.get(`http://localhost:3001/product/all`, {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        if(((res.data.data).length) === 0){
+            return 
+
+          } else {
+            setProducts(res.data.data);
+          }
+      } catch(err){
+        console.log(err)
+      }
+    }
+    getProducts();
 
   }, [cat])
 
   return (
     <Container>
-        {popularProducts.map(item => (
+        {products.map(item => (
             <Product key={item.id} item={item} />
          ))}
     </Container>
